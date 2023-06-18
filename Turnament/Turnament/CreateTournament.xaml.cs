1c7e982e1fd2df5cb108d5cdd57db8960382c4f5
Dawid Gramacki <80342@student.pb.edu.pl>
2023-06-18 19:25:11 +0200

    Widok Menu i torchę nawigacji

---
+++ Turnament/Turnament/CreateTournament.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Turnament
+{
+    /// <summary>
+    /// Logika interakcji dla klasy CreateTournament.xaml
+    /// </summary>
+    public partial class CreateTournament : Window
+    {
+        public CreateTournament()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClickCreate(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            MainWindow mainWindow = new MainWindow(); //Trzeba dodać parametr do okna main window z ID turnieju 
+            mainWindow.Show();
+        }
+
+        private void BtnClickBack(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Menu menu = new Menu();
+            menu.Show();
+        }
+    }
+}
---
+++ Turnament/Turnament/Menu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Turnament
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnClickCreateTournament(object sender, RoutedEventArgs e)
+        {
+            CreateTournament ct = new CreateTournament();
+            this.Hide();
+            ct.Show();
+        }
+
+        private void BtnClickSelectTournament(object sender, RoutedEventArgs e)
+        {
+            SelectTournament st = new SelectTournament();
+            this.Hide();
+            st.Show();
+        }
+    }
+}
