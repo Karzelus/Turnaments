42b396317e43f1486d270c0e6b9ed4698de4f49b
eldawidos1111 <eldawidos111@gmail.com>
2023-06-21 19:29:57 +0200

    Select Tournament View

--- Turnament/Turnament/SelectTournament.xaml.cs
+++ Turnament/Turnament/SelectTournament.xaml.cs
@@ -11,17 +11,25 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Turnaments.Models;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace Turnament
 {
     /// <summary>
     /// Logika interakcji dla klasy SelectTournament.xaml
     /// </summary>
+    
     public partial class SelectTournament : Window
     {
         public SelectTournament()
         {
             InitializeComponent();
+            string filePath = @"C:\JSON\turnamentSerialized.json";
+            var jsonData = File.ReadAllText(filePath);
+            List<Turnaments.Models.Turnament> turnamentList = JsonConvert.DeserializeObject<List<Turnaments.Models.Turnament>>(jsonData);
+            Tournaments.ItemsSource = turnamentList;
         }
 
         private void BtnClickSelect(object sender, RoutedEventArgs e)
